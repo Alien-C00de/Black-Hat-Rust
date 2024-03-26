bef417cd39a2212f27c8b78e5b9f3da51f2ce98a
VBU-PGDCD <alien.c00de+1@gmail.com>
2024-03-26 19:07:28 +0530

    Updated changes

--- sha1_cracker/src/main.rs
+++ sha1_cracker/src/main.rs
@@ -1,39 +1,39 @@
 use sha1::Digest;
 use std::{
-    env::{self, args}, 
-    error::Error, 
-    fs::File, 
+    env::{self, args},
+    error::Error,
+    fs::File,
     io::{BufRead, BufReader},
 };
 
 const SHA1_HEX_STRING_LENGTH: usize = 40;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String>  = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
     if args.len() != 3 {
         println!("Usage");
         println!("sha1_cracker: <wordlist.txt> <sha1_bash>");
         return Ok(());
-    }   
+    }
 
     let hash_to_crack = args[2].trim();
     if hash_to_crack.len() != SHA1_HEX_STRING_LENGTH {
-        return  Err("sha1 hash is not valid.".into());
+        return Err("sha1 hash is not valid.".into());
     }
 
-    let worldlist_file = File::open(&args[1])?;
-    let reader = BufReader::new(&worldlist_file);
+    let wordlist_file = File::open(&args[1])?;
+    let reader = BufReader::new(&wordlist_file);
 
-    for line in reader.lines(){
-        let line  = line?;
+    for line in reader.lines() {
+        let line = line?;
         let common_password = line.trim();
 
-        if hash_to_crack == &hex::encode(sha1::Sha1::digest(common_password.as_bytes())){
+        if hash_to_crack == &hex::encode(sha1::Sha1::digest(common_password.as_bytes())) {
             println!("Password found: {}", &common_password);
-            return  Ok(());
+            return Ok(());
         }
-        println!("{}", line);
+        // println!("{}", line);
     }
     println!("Password not found in the word list");
 
---
+++ tricoder/target/debug/deps/tricoder-4f547256528f60a3.d
@@ -0,0 +1,5 @@
+/home/sanu/Documents/santosh/rust/Black-Hat-Rust/tricoder/target/debug/deps/libtricoder-4f547256528f60a3.rmeta: src/main.rs
+
+/home/sanu/Documents/santosh/rust/Black-Hat-Rust/tricoder/target/debug/deps/tricoder-4f547256528f60a3.d: src/main.rs
+
+src/main.rs:
